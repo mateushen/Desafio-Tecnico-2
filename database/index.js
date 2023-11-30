86039fedcffb4e48d5b582eeb252973a44264a11
Mateus Henrique <112947808+mateushen@users.noreply.github.com>
2023-11-30 19:06:18 -0300

    Conexão com o banco

--- database/index.js
+++ database/index.js
@@ -1,1 +1,16 @@
-qqqq
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('api', 'root', 'root', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Conexão bem-sucedida.');
+} catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+}
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const path = require('path');
 const bp = require('body-parser');
 const session = require('express-session');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const dbconnection = require('./database/index.js');
+const dbconnection = require('./database/index');
 const app = express();
 
 app.use(express.json());
 
-const despesa = require('./routes/despesa');
-const funcionario = require('./routes/funcionario');
-const obra = require('./routes/obra');
-const servico = require('./routes/servico');
-const usuario = require('./routes/usuario');
+const route = require('./routes/index');
+
+app.use('/user', route);
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
@@ -22,32 +20,17 @@ var sessionStore = new SequelizeStore({
   db: dbconnection,
 });
 
-const oneDay = 1000 * 60 * 60 * 24;
+const thirtyMinutes = 1000 * 60 * 30;
 app.use(session({
   secret: "123456789",
   saveUninitialized: true,
-  cookie: { maxAge: oneDay },
+  cookie: { maxAge: thirtyMinutes },
   resave: false,
   store: sessionStore
 }));
 
 sessionStore.sync();
 
-
-const staticPath = path.join(__dirname, 'public');
-app.use(express.static(staticPath));
-
-
-app.get('/', (req, res) => {
-  checkUsuario().then((response) => {
-    if (response) {
-      res.render('usuario/login');
-    } else {
-      res.render('usuario/cadastro');
-    }
-  })
-});
-
 app.listen(3000, () => {
-  console.log('Server rodando')
+  console.log('Server rodando');
 });
